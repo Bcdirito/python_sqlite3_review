b65681b4f3e50af8b0c235d9219704809fd44345
Bcdirito <bdirito91@gmail.com>
2019-08-25 15:37:12 -0400

    made api request

---
+++ python_api_review.py
@@ -0,0 +1,35 @@
+import requests
+import random
+import json
+
+met_url = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
+
+initial_get_request = requests.get(met_url)
+get_request_content = ""
+object_keys = []
+
+if initial_get_request.status_code == 200:
+    get_request_content = json.loads(initial_get_request.content)
+    for key in get_request_content.keys():
+        object_keys.append(key)
+    print(f"Request Successful:{object_keys}")
+else:
+    print("Request Failed")
+
+object_ids = object_keys[-1]
+
+def get_random_tour_ids(content, ids_key):
+    random.shuffle(content[ids_key])
+    return content[ids_key][0:10]
+
+def get_random_tour_content(ids):
+    art_objects_list = []
+    for obj in ids:
+        response = requests.get(f"{met_url}/{obj}")
+        if response.status_code == 200:
+            art_objects_list.append(json.loads(response.content))
+            
+    return art_objects_list
+        
+print(get_random_tour_content(get_random_tour_ids(get_request_content, object_ids)))
+    
